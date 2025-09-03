75e06611b8fd7794c02702bd4dcb0f34c2062341
Char701 <char701@aucklanduni.ac.nz>
2025-09-03 17:00:57 +1200

    Frontend setup
    Additional front end set up

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className="bg-[#F5F5F5]">
+            <div>
+                <p>This is where the logo will go</p>
+            </div>
+            <div>
+                <h1>Title</h1>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
